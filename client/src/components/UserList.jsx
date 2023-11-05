0dd891852ec4671d0dcbc8fa95dcf12f69e3e609
dsimplichev <dsimplichev1948@gmail.com>
2023-11-05 15:12:31 +0000

    add butn add new user

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,9 +1,13 @@
 import Search from "./Search"
+import UserListTable from './UserListTable';
 
 const UserList = () => {
     return (
         <section className="card users-container">
             <Search />
+            
+            <UserListTable />
+            <button class="btn-add btn">Add new user</button>
         </section>
 
     )
